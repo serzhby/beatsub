0eaf1640d6c51a4b9452728841a8380fe4172fb0
sergey <sergey.zhabinskii@gmail.com>
2016-07-23 02:21:41 +0300

    Add deserialization of subsonic response

--- src/main/java/by/serzh/beatsub/api/client/ResponseExtractor.java
+++ src/main/java/by/serzh/beatsub/api/client/ResponseExtractor.java
@@ -1,40 +1,50 @@
 package by.serzh.beatsub.api.client;
 
+import by.serzh.beatsub.api.domain.SubsonicError;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 public class ResponseExtractor {
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    public static <T> T extract(InputStream stream, Class<T> cl) throws IOException {
-        Response response = objectMapper.readValue(stream, Response.class);
-        SubsonicResponse<T> subsonicResponse = response.<T>getResponse();
-        return subsonicResponse.content;
+    static {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.findAndRegisterModules();
     }
 
-    private static class Response<T> {
-        private SubsonicResponse<T> response;
+    public static <T> T extract(InputStream stream, Class<T> cl) throws IOException {
+        TypeReference<Map<String, Object>> typeRef = new TypeReference<Map<String, Object>>() {};
+        ObjectMapper mapper = new ObjectMapper();
+        T result = null;
 
-        @JsonGetter("subsonic-response")
-        public SubsonicResponse<T> getResponse() {
-            return response;
+        Map<String, Object> data = mapper.readValue(stream, typeRef);
+        Map<String, Object> subsonicResponse = (Map<String, Object>) data.get("subsonic-response");
+        Status status = objectMapper.convertValue(subsonicResponse.get("status"), Status.class);
+        if(status == Status.OK) {
+            Object object = subsonicResponse.get(cl.getSimpleName().toLowerCase());
+            if(object != null) {
+                result = objectMapper.convertValue(object, cl);
+            }
+        } else {
+            SubsonicError error = objectMapper.convertValue(subsonicResponse.get("error"), SubsonicError.class);
         }
 
-        @JsonSetter("subsonic-response")
-        public void setResponse(SubsonicResponse<T> response) {
-            this.response = response;
-        }
+        return result;
     }
 
     private static class SubsonicResponse<T> {
         private Status status;
         private String version;
+        private SubsonicError error;
         private T content;
 
         @JsonGetter("status")
